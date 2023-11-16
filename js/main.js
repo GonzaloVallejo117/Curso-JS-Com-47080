74bfd2d3106ea34776f5f52b14da57fe28286959
GonzaloVallejo117 <gonzav007@gmail.com>
2023-11-15 23:47:47 -0300

    se agrego fetch y archico Turnos_precargados.json con el cual trabaja

--- js/main.js
+++ js/main.js
@@ -1,76 +1,95 @@
 //validacion de que no se pueda acceder al index sin estar logeado
-const usuario = JSON.parse(localStorage.getItem("login_exitoso")) || false
-if(!usuario){
-  window.location.href = "login.html"
+const usuario = JSON.parse(localStorage.getItem("login_exitoso")) || false;
+if (!usuario) {
+  window.location.href = "login.html";
 }
 
 const cerrarSesion = document.querySelector("#cerrarSesion");
-cerrarSesion.addEventListener("click", ()=>{
+cerrarSesion.addEventListener("click", () => {
   Swal.fire({
-    title: "Desea cerrar la sesión?",
+    title: "¿Desea cerrar la sesión?",
     icon: "warning",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
-    confirmButtonText: "Cerrar sesión"
+    confirmButtonText: "Cerrar sesión",
   }).then((result) => {
     if (result.isConfirmed) {
       Swal.fire({
         title: "Sesión finalizada",
-        icon: "success"
+        icon: "success",
       });
       localStorage.removeItem('login_exitoso');
       setTimeout(() => {
-        window.location.href = "login.html"
+        window.location.href = "login.html";
       }, 2000);
     }
-  })
-})
+  });
+});
 
+let turnosPrecargados = [];
+let btnPrecargar = document.querySelector("#turnosPrecargados");
 
+//FETCH
+fetch("./js/turnos_precargados.json")
+  .then((respuesta) => respuesta.json())
+  .then((data) => {
+    turnosPrecargados = data;
+    // evento para cargar los turnos precargados
+  btnPrecargar.addEventListener("click", function () {
+    Swal.fire({
+      title: "Desea mostrar los Turnos Precargados?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Si",
+      denyButtonText: "No",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Turnos cargados con éxito", "", "success");
+        turnosGenerados = turnosGenerados.concat(turnosPrecargados);
+        localStorage.setItem("turnosGenerados", JSON.stringify(turnosGenerados));
+        mostrarTurnos();
+      }
+    });
+  });
+  });
 
-
-//simulador de turnos medicos
-
-
-//codigo que usare para crear los turnos
-class PersonaTurno{
-    
-  constructor (nombre, apellido, dni, especialidad,diaTurno,horaTurno){
-  this.nombre = nombre;
-  this.apellido = apellido;
-  this.dni = dni;
-  this.especialidad=especialidad;
-  this.diaTurno = diaTurno;
-  this.horaTurno = horaTurno;
+// simulador de turnos medicos
+class PersonaTurno {
+  constructor(nombre, apellido, dni, especialidad, diaTurno, horaTurno) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.especialidad = especialidad;
+    this.diaTurno = diaTurno;
+    this.horaTurno = horaTurno;
   }
 }
 
-function imprimirTurno(PersonaTurno, indice){
+// función que crea el div con la info del turno
+function imprimirTurno(PersonaTurno, indice) {
   let container = document.createElement("div");
-  container.className="turnoGenerado";
+  container.className = "turnoGenerado";
   container.innerHTML = `
-  <p>Nombre: ${PersonaTurno.nombre}</p>
-  <p>Apellido: ${PersonaTurno.apellido}</p>
-  <p>Dni: ${PersonaTurno.dni}</p>
-  <p>Especialidad: ${PersonaTurno.especialidad}</p>
-  <p>Día: ${PersonaTurno.diaTurno}</p>
-  <p>Hora: ${PersonaTurno.horaTurno}</p>
-  <button class="eliminarTurno" data-indice="${indice}">Eliminar</button>
+    <p>Nombre: ${PersonaTurno.nombre}</p>
+    <p>Apellido: ${PersonaTurno.apellido}</p>
+    <p>Dni: ${PersonaTurno.dni}</p>
+    <p>Especialidad: ${PersonaTurno.especialidad}</p>
+    <p>Día: ${PersonaTurno.diaTurno}</p>
+    <p>Hora: ${PersonaTurno.horaTurno}</p>
+    <button class="eliminarTurno" data-indice="${indice}">Eliminar</button>
   `;
 
-  //agrega un evento al boton de eliminar
-
+  // agrega un evento al boton de eliminar
   let btnEliminar = container.querySelector(".eliminarTurno");
-  btnEliminar.addEventListener("click", function(){
+  btnEliminar.addEventListener("click", function () {
     Swal.fire({
       title: "Desea eliminar el turno?",
       showDenyButton: true,
       showCancelButton: true,
       confirmButtonText: "Si",
-      denyButtonText: `No`
+      denyButtonText: `No`,
     }).then((result) => {
-      /* Read more about isConfirmed, isDenied below */
       if (result.isConfirmed) {
         Swal.fire("Turno eliminado", "", "success");
         eliminarTurno(indice);
@@ -78,18 +97,17 @@ function imprimirTurno(PersonaTurno, indice){
         Swal.fire("El turno no ha sido eliminado", "", "info");
       }
     });
-  })
+  });
   return container;
 }
 
-//llamo al form
+// llamado al form
 const registroForm = document.getElementById("registroForm");
 const listaTurnos = document.getElementById("pacientesRegistrados");
 const contenedorTurnos = document.getElementById("contenedor_turnos");
 
 // array donde se van a almacenar los turnos generados
 let turnosGenerados = [];
-
 
 // Recupera la información almacenada en el localStorage
 if (localStorage.getItem("turnosGenerados")) {
@@ -100,54 +118,60 @@ if (localStorage.getItem("turnosGenerados")) {
 
 // evento para manejar el envío del formulario
 registroForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita el envío del formulario
-    
-    Swal.fire({
-      icon: "success",
-      title: "Turno generado con exito",
+  event.preventDefault(); // Evita el envío del formulario
+
+  Swal.fire({
+    icon: "success",
+    title: "Turno generado con éxito",
   });
-    // obtengo los valores del formulario
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const dni = document.getElementById("dni").value;
-    const especialidad = document.getElementById("especialidad").value;
-    const diaTurno = document.getElementById("diaTurno").value;
-    const horaTurno = document.getElementById("horaTurno").value;
-    
-    // Construye el mensaje con los datos del turno
-    let nuevoTurno = new PersonaTurno(nombre,apellido,dni,especialidad,diaTurno,horaTurno)
-    
-    turnosGenerados.push(nuevoTurno);
-    // Almacena la información en el localStorage
-    localStorage.setItem("turnosGenerados", JSON.stringify(turnosGenerados));
 
-    mostrarTurnos();
-    
-    registroForm.reset();
+  // obtengo los valores del formulario
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const dni = document.getElementById("dni").value;
+  const especialidad = document.getElementById("especialidad").value;
+  const diaTurno = document.getElementById("diaTurno").value;
+  const horaTurno = document.getElementById("horaTurno").value;
+
+  // Construye el mensaje con los datos del turno
+  let nuevoTurno = new PersonaTurno(
+    nombre,
+    apellido,
+    dni,
+    especialidad,
+    diaTurno,
+    horaTurno
+  );
+
+  turnosGenerados.push(nuevoTurno);
+  // Almacena la información en el localStorage
+  localStorage.setItem("turnosGenerados", JSON.stringify(turnosGenerados));
+
+  mostrarTurnos();
+
+  registroForm.reset();
+});
+
+// función para mostrar los turnos
+function mostrarTurnos() {
+  // limpia el contenedor antes de mostrar los turnos
+  contenedorTurnos.innerHTML = "";
+  // muestra los turnos almacenados
+  turnosGenerados.forEach((turno, indice) => {
+    // crea un elemento div que va a ser el contenedor del turno
+    let divTurno = imprimirTurno(turno, indice);
+    contenedorTurnos.appendChild(divTurno);
   });
-  
-  function mostrarTurnos(){
-    //limpia el contenedor antes de mostar los turnos
-    contenedorTurnos.innerHTML = "";
-    //muestra los turnos almacenados
-    turnosGenerados.forEach((turno,indice) =>{
-      //crea un elemento div que va a ser el contenedor del turno
-      let divTurno = imprimirTurno(turno,indice);
-      contenedorTurnos.appendChild(divTurno);
-    });
-  }
-  function eliminarTurno(indice) {
-    // Elimina el turno del array
-    turnosGenerados.splice(indice, 1);
-  
-    // Actualiza el localStorage
-    localStorage.setItem("turnosGenerados", JSON.stringify(turnosGenerados));
-  
-    // Muestra los turnos actualizados
-    mostrarTurnos();
-  }
+}
 
+// función para eliminar un turno
+function eliminarTurno(indice) {
+  // Elimina el turno del array
+  turnosGenerados.splice(indice, 1);
 
+  // Actualiza el localStorage
+  localStorage.setItem("turnosGenerados", JSON.stringify(turnosGenerados));
 
-
-
+  // Muestra los turnos actualizados
+  mostrarTurnos();
+}
